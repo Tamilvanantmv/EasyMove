a5d119470d180d1fc88bb508f96667e424f65bcb
Tamil <tamilvanantmv@gmail.com>
2024-01-31 23:30:40 +0530

    Trying to setup from beginning

--- app/src/main/java/com/example/easymove/screens/MainScreen.kt
+++ app/src/main/java/com/example/easymove/screens/MainScreen.kt
@@ -1,12 +1,19 @@
 package com.example.easymove.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Place
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,33 +22,49 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.easymove.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun MainScreen(navController: NavController) {
+
     Surface(modifier = Modifier.fillMaxSize(), elevation = 10.dp) {
 
         Column() {
             Row(modifier = Modifier.fillMaxSize()) {
-
-                Column() {
-                    TextField(value = "From", onValueChange = {}, modifier = Modifier.height(50.dp))
-                }
-
                 Image(
                     painter = painterResource(id = R.drawable.from_to),
                     contentDescription = "Source-destination image",
                     contentScale = ContentScale.FillWidth,
                     alignment = Alignment.Center,
-                    modifier = Modifier.size(250.dp)
+                    modifier = Modifier.size(50.dp)
                 )
-            }
-            Row(modifier = Modifier.fillMaxSize()) {
+
                 Column() {
-                    TextField(value = "To", onValueChange = {}, modifier = Modifier.height(50.dp))
+                    Row() {
+                        var fromText: String by remember { mutableStateOf("") }
+                        TextField(
+                            value = fromText,
+                            onValueChange = { fromText = it },
+                            modifier = Modifier.height(50.dp)
+                        )
+                        Icon(Icons.Outlined.Place, contentDescription = "Testing the place", modifier = Modifier.clickable {
+                            Log.i("Location", "From location")
+                        }.fillMaxWidth().size(50.dp))
+                    }
+                    Row() {
+                        var toText by remember { mutableStateOf("") }
+                        TextField(
+                            value = "my text",
+                            onValueChange = { toText = it },
+                            modifier = Modifier.height(50.dp)
+                        )
+                        Icon(Icons.Outlined.Place, contentDescription = "Testing the place", modifier = Modifier.clickable {
+                            openMap() }.fillMaxWidth().size(50.dp))
+                    }
                 }
             }
         }
 
     }
-
 }
---
+++ app/src/main/java/com/example/easymove/screens/MapScreen.kt
@@ -0,0 +1,7 @@
+package com.example.easymove.screens
+
+import android.util.Log
+
+fun openMap() {
+    Log.d("Map", "Opening the map")
+}
